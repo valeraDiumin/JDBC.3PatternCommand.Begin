5a4355d6403744fc14728813034b6569de4fe1fa
valeraDiumin <valera_diumin@ukr.net>
2016-03-07 23:47:04 +0200

    Layer JDBC have done. Started next lection Stories. Main deleted.Interface added.

--- test/JDBCDataBaseManagerTest.java
+++ test/JDBCDataBaseManagerTest.java
@@ -6,29 +6,29 @@ import java.util.Arrays;
 import static junit.framework.TestCase.assertEquals;
 
 
-public class DataBaseManagerTest {
+public class JDBCDataBaseManagerTest {
 
-    private DataBaseManager dataBaseManager;
+    private JDBCDataBaseManager JDBCDataBaseManager;
     private String tableName1;
 
     @Before
     public void setup() {
-        dataBaseManager = new DataBaseManager();
-        dataBaseManager.connect("postgres", "postgres", "11111111");
-        dataBaseManager.getConnection();
+        JDBCDataBaseManager = new JDBCDataBaseManager();
+        JDBCDataBaseManager.connect("postgres", "postgres", "11111111");
+        JDBCDataBaseManager.getConnection();
         tableName1 = "user12";
     }
 
     @Test
     public void listOfAllTablesTest() {
-        String[] listOfAllTables = dataBaseManager.listOfAllTables();
+        String[] listOfAllTables = JDBCDataBaseManager.listOfAllTables();
         assertEquals("[user1, user2, user10, user11, user12, user]", Arrays.toString(listOfAllTables));
     }
 
     @Test
     public void getTableDataTest() {
 //        give table from base and prepare it for test
-        dataBaseManager.clearTable(tableName1);
+        JDBCDataBaseManager.clearTable(tableName1);
 
         // when createStringInTable string in base
         DataSet data = new DataSet();
@@ -36,10 +36,10 @@ public class DataBaseManagerTest {
         data.put("NAME", "Jack Bob");
         data.put("SALARY", "1000000");
         //put data to DataSet object
-        dataBaseManager.createStringInTable(data, tableName1); // I have to give to method the name of table or hardcode it to method createStringInTable
+        JDBCDataBaseManager.createStringInTable(data, tableName1); // I have to give to method the name of table or hardcode it to method createStringInTable
 
         //give data from base and test array of data length (amount of strings in data)
-        DataSet[] users = dataBaseManager.getTableData(tableName1);
+        DataSet[] users = JDBCDataBaseManager.getTableData(tableName1);
 
 
         assertEquals(1, users.length);
@@ -54,14 +54,14 @@ public class DataBaseManagerTest {
     }
     @Test
     public void getSizeTest() {
-        int size = dataBaseManager.getSize(tableName1);
+        int size = JDBCDataBaseManager.getSize(tableName1);
         assertEquals("1", "" + size);
     }
     @Test
     public void updateTest() {
 
         //        give table from base and prepare it for test
-        dataBaseManager.clearTable(tableName1);
+        JDBCDataBaseManager.clearTable(tableName1);
 
         // when createStringInTable string in base
         DataSet updateData = new DataSet();
@@ -69,7 +69,7 @@ public class DataBaseManagerTest {
         updateData.put("NAME", "Jack Bob");
         updateData.put("SALARY", "1000000");
         //put data to DataSet object
-        dataBaseManager.createStringInTable(updateData, tableName1); // I have to give to method the name of table or hardcode it to method createStringInTable
+        JDBCDataBaseManager.createStringInTable(updateData, tableName1); // I have to give to method the name of table or hardcode it to method createStringInTable
 
         DataSet updateData1 = new DataSet();
 //        updateData1.put("id", 3);
@@ -77,10 +77,10 @@ public class DataBaseManagerTest {
         updateData1.put("SALARY", "1000"); // to DataSet
 
         // begin with string in table from previous tests
-        dataBaseManager.updateFromDataSet(tableName1, updateData1, 3);// Directly to Postgresql base
+        JDBCDataBaseManager.updateFromDataSet(tableName1, updateData1, 3);// Directly to Postgresql base
         //give data from base and test array of data length (amount of strings in data)
 
-        DataSet[] users = dataBaseManager.getTableData(tableName1);
+        DataSet[] users = JDBCDataBaseManager.getTableData(tableName1);
 
         assertEquals(1, users.length);
 
