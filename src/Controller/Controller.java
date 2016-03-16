a2593b0fd5b61a402121fc1386fb1063910aa6d4
valeraDiumin <valera_diumin@ukr.net>
2016-03-16 22:59:09 +0200

    Doing Second lection "Stories &....". 1,5 h. Created and tested connect, clean, print, create strings to table in database. Have added "failed connection massage" like lection. Applied DIP to controller, clean, readTable, insertStringToTable

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,54 +1,50 @@
 package Controller;
 
-import View.Console;
 import View.Viewshka;
+import connectAndCommands.DataBaseManager;
 import connectAndCommands.DataSet;
-import connectAndCommands.JDBCDataBaseManager;
-import connectAndCommands.inMemoryDataBaseManager;
-
 import java.sql.SQLException;
 import java.util.Arrays;
 
 public class Controller {
-    public static void main(String[] args) {
-        JDBCDataBaseManager jdbcDataBaseManager = new JDBCDataBaseManager();
-        inMemoryDataBaseManager inMemoryDataBaseManager = new inMemoryDataBaseManager();
-        DataSet dataSet = new DataSet();
-        Viewshka controller = new Console();
+    DataBaseManager jdbcDataBaseManager;
+    DataSet dataSet;
+    Viewshka controller;
+    String tableName;
 
-        controller.wright("Юзер, привет");
 
-        //connection block
-        String baseName;
-        while (true) {
-            controller.wright("Пожалуйста, введите логин, пароль и имя базы в формате логин|пароль|база ");
-            String s = controller.read();
-            String[] strings = s.split("\\|");
-            String login = strings[0];
-            String parole = strings[1];
-            baseName = strings[2];
-            try {
-                jdbcDataBaseManager.connect(login, parole, baseName);
-                break;
-            } catch (SQLException e) {
-                String connectMassage = e.getMessage();
-                if (e.getCause() != null) {
-                    connectMassage += e.getCause().getMessage();
-                }
-                controller.wright("Подключение к базе данных " + baseName + " не состоялось :( по причине: \n" + connectMassage);
-                controller.wright("Пожалуйста, повторите попытку");
+    public Controller(DataBaseManager jdbcDataBaseManager, Viewshka controller) {
+        this.jdbcDataBaseManager = jdbcDataBaseManager;
+        dataSet = new DataSet();
+        this.controller = controller;
+    }
+
+    public void run(){
+        connectToDataBase();
+        this.tableName = getTableName();
+        changingTable(tableName);
+        readAllTable(tableName);
+    }
+
+    private void readAllTable(String tableName) {
+        controller.wright("Вы желаете посмотреть содержимое всей таблицы '" + tableName + "' ? Y/N");
+        String read = controller.read();
+        if (read.equals("Y")) {
+            DataSet[] dataSet1 = jdbcDataBaseManager.getTableData(tableName);
+
+            for (DataSet aDataSet1 : dataSet1) {//проходим по всем строкам
+
+
+                System.out.println(Arrays.toString(aDataSet1.getColumnNames()));
+                System.out.println(Arrays.toString(aDataSet1.getColumnValues()));
+                System.out.println("");
+
             }
+            controller.wright("Содержимое таблицы '" + tableName + "' показано");
         }
-        controller.wright("Вы успешно подсоединились к базе данных " + baseName + " !");
+    }
 
-
-        //preparing to changing block
-        controller.wright("С какой таблицей Вы желаете работать? Пожалуйста, введите название таблицы");
-        String tableName = controller.read();
-        controller.wright("Таблица '" + tableName + "' выбрана для работы");
-
-
-        //changing block
+    private void changingTable(String tableName) {
         controller.wright("Вы желаете изменить содержание таблицы '" + tableName + "' ? Y/N");
         String iWishToChangeTable = controller.read();
         if (iWishToChangeTable.equals("Y")) {
@@ -76,22 +72,40 @@ public class Controller {
                 controller.wright("Строка данных успешно добавлена в таблицу '" + tableName + "' !");
             }
         }
+    }
 
-        controller.wright("Вы желаете посмотреть содержимое всей таблицы '" + tableName + "' ? Y/N");
-        String read = controller.read();
-        if (read.equals("Y")) {
-            DataSet[] dataSet1 = jdbcDataBaseManager.getTableData(tableName);
+    private String getTableName() {
+        controller.wright("С какой таблицей Вы желаете работать? Пожалуйста, введите название таблицы");
+        String tableName = controller.read();
+        controller.wright("Таблица '" + tableName + "' выбрана для работы");
+        return tableName;
+    }
 
-            for (DataSet aDataSet1 : dataSet1) {//проходим по всем строкам
+    private void connectToDataBase() {
+        controller.wright("Юзер, привет");
 
-
-                System.out.println(Arrays.toString(aDataSet1.getColumnNames()));
-                System.out.println(Arrays.toString(aDataSet1.getColumnValues()));
-                System.out.println("");
-
+        //connection block
+        String baseName;
+        while (true) {
+            controller.wright("Пожалуйста, введите логин, пароль и имя базы в формате логин|пароль|база ");
+            String s = controller.read();
+            String[] strings = s.split("\\|");
+            String login = strings[0];
+            String parole = strings[1];
+            baseName = strings[2];
+            try {
+                jdbcDataBaseManager.connect(login, parole, baseName);
+                break;
+            } catch (SQLException e) {
+                String connectMassage = e.getMessage();
+                if (e.getCause() != null) {
+                    connectMassage += e.getCause().getMessage();
+                }
+                controller.wright("Подключение к базе данных " + baseName + " не состоялось :( по причине: \n" + connectMassage);
+                controller.wright("Пожалуйста, повторите попытку");
             }
-            controller.wright("Содержимое таблицы '" + tableName + "' показано");
         }
+        controller.wright("Вы успешно подсоединились к базе данных " + baseName + " !");
     }
 }
 
---
+++ src/Controller/Main.java
@@ -0,0 +1,23 @@
+package Controller;
+
+import View.Console;
+import View.Viewshka;
+import connectAndCommands.DataBaseManager;
+import connectAndCommands.JDBCDataBaseManager;
+import connectAndCommands.inMemoryDataBaseManager;
+
+/**
+ * Created by 123 on 16.03.2016.
+ */
+public class Main {
+
+
+    public static void main(String[] args) {// создали менеджер и вьюху, которые используем, потом Контроллер, куда их уложили в конструктор,
+        DataBaseManager jdbcDataBaseManager = new JDBCDataBaseManager();
+        DataBaseManager inMemoryDataBaseManager = new inMemoryDataBaseManager();
+        Viewshka viewshka = new Console();
+
+        Controller controller = new Controller(jdbcDataBaseManager, viewshka);
+        controller.run(); // запускаем ран, и вуаля!!!
+    }
+}
